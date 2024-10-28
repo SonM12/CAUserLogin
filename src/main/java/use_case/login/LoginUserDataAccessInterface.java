78bd4d95ab6427b220f6d7f75625f02034f9f66b
Mingyu Son <mingyuson@Mingyus-MacBook-Air.local>
2024-10-28 15:19:45 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set Current User method.
+     * @param name name
+     */
+    void setCurrentUser(String name);
 }
